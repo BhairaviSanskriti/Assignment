f5fd47483921f6e297313b491700a903b2e22322
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-02 21:17:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+}
 provider "aws"{
     region = "ap-south-1"
 }
-
 resource "aws_instance" "jenkins-ec2-instance"{
     ami = "ami-0cca134ec43cf708f" #Image id for the ap-south-1 region
     instance_type = "t2.micro"
