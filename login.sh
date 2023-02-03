4afb459dc8ea4eb4eb1fd2dacc8927cf783d4c2c
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 17:08:22 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+
+CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
+
+kubectl config use-context $CONTEXT_NAME
+
 export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
 
 export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
 
 argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
-
-
-CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
-
-kubectl config use-context $CONTEXT_NAME
 
 argocd cluster add $CONTEXT_NAME
 
