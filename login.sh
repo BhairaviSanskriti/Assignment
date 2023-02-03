5790a0aeedbbb0304149172343ea0ef4d00241b9
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 17:39:10 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -4,13 +4,13 @@
 # CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
 
 
-CONTEXT_NAME='kubectl config get-contexts -o name'
+CONTEXT_NAME=`kubectl config get-contexts -o name`
 
 kubectl config use-context $CONTEXT_NAME
 
-export ARGOCD_SERVER='kubectl get svc argocd-server -n argocd -o json | jq --raw-output \'.status.loadBalancer.ingress[0].hostname\''
+export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
 
-export ARGO_PWD='kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d'
+export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
 
 argocd cluster add $CONTEXT_NAME
 
