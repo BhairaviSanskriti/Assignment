8ecea03eb89c91e26b306bf51f7c1420d585433e
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 13:51:16 +0530

    Update deploy_nginx.sh

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -4,6 +4,6 @@ kubectl create namespace nginx-app
 
 argocd app create nginx-application --repo https://github.com/BhairaviSanskriti/Test-Jenkins.git --path deployments/nginx --dest-server https://kubernetes.default.svc --dest-namespace nginx-app
 
-argocd app sync nginx-application
+# argocd app sync nginx-application
 
 kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'
