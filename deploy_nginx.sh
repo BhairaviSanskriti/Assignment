a8bc8755224b09138f65bc8e3c4ab083a59a951e
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 13:45:22 +0530

    Update deploy_nginx.sh

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -2,7 +2,7 @@
 
 kubectl create namespace nginx-app
 
-argocd app create nginx-application --repo https://github.com/BhairaviSanskriti/Test-Jenkins.git --path ./deployments/nginx --dest-server https://kubernetes.default.svc --dest-namespace nginx-app
+argocd app create nginx-application --repo https://github.com/BhairaviSanskriti/Test-Jenkins.git --path deployments/nginx --dest-server https://kubernetes.default.svc --dest-namespace nginx-app
 
 argocd app sync nginx-application
 
