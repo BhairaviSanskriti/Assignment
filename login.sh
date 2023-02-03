9a45cc50acd9e08c0cbdc7d4ac0df4c0e1aa610e
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 16:56:12 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -6,14 +6,10 @@ export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jso
 
 argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
 
-echo 'Logged in to the server'
 
 CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
-
-echo "$CONTEXT_NAME: this context is to be added"
+argocd cluster add $CONTEXT_NAME
 
 # argocd cluster add $(terraform output -raw cluster_name) --server $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
 
-argocd cluster add $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
-
-echo "context is added"
+# argocd cluster add $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
