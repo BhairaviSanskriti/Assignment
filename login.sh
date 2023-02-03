94a60b5dfa2391c4d510e41d027a6bec1ddf6638
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-03 17:27:11 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 
-CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
+# CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
+
+
+CONTEXT_NAME='kubectl config get-contexts -o name'
 
 kubectl config use-context $CONTEXT_NAME
 
-export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
+export ARGOCD_SERVER='kubectl get svc argocd-server -n argocd -o json | jq --raw-output \'.status.loadBalancer.ingress[0].hostname\''
 
-export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+export ARGO_PWD='kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d'
+
+argocd cluster add $CONTEXT_NAME
 
 argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
 
-argocd cluster add $CONTEXT_NAME
 
 # argocd cluster add $(terraform output -raw cluster_name) --server $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
 
