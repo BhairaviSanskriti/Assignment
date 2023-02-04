3f9ba4250b81fb0d1c8e8ac9c300c8cb3c255198
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-04 13:49:28 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-# CONTEXT_NAME=`kubectl config view -o jsonpath='{.current-context}'`
-
-
 CONTEXT_NAME=`kubectl config get-contexts -o name`
 
 kubectl config use-context $CONTEXT_NAME
@@ -15,10 +11,3 @@ export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jso
 argocd cluster add $CONTEXT_NAME -y
 
 argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
-
-
-# argocd cluster add $(terraform output -raw cluster_name) --server $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
-
-# argocd cluster add $(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
-
-
