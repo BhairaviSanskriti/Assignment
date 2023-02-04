04f4250c7e2157cce217df4bb3825f3fa24f1e60
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-04 13:49:52 +0530

    Update deploy_nginx.sh

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -3,6 +3,6 @@
 
 kubectl create namespace nginx-ns
 
-argocd app create nginx-app --repo https://github.com/BhairaviSanskriti/Assignment.git --path deployments/nginx --dest-server https://kubernetes.default.svc --dest-namespace nginx-ns
+argocd app create nginx-app --repo https://github.com/BhairaviSanskriti/Assignment.git --path deployments --dest-server https://kubernetes.default.svc --dest-namespace nginx-ns
 
 argocd app sync nginx-app
